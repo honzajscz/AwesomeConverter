26a6a24a545a250ded3cf2ea8ce24e1edda9d963
Jan Vratislav <jan.vratislav@vertigis.com>
2021-11-12 13:49:00 +0100

    Initial commit

---
+++ AwesomeConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AwesomeConverter.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public double? LengthInMiles { get; set; }
+
+    public double? LengthInMeters { get; set; }
+
+    public void OnPost()
+    {
+        if (LengthInMiles == null)
+            return;
+
+        LengthInMeters = ConvertMilesToMeters(LengthInMiles.Value);
+    }
+
+    private static double ConvertMilesToMeters(double lengthInMiles)
+    {
+        double metersInMiles = 1_600;
+
+        return lengthInMiles * metersInMiles;
+    }
+}
---
+++ AwesomeConverterTests/IndexModelTests.cs
@@ -0,0 +1,24 @@
+﻿using AwesomeConverter.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AwesomeConverterTests;
+
+[TestClass()]
+public class IndexModelTests
+{
+    [TestMethod()]
+    public void OnPostTest()
+    {
+        // arrange 
+        IndexModel indexModel = new ()
+        {
+            LengthInMiles = 10
+        };
+
+        // act 
+        indexModel.OnPost();
+
+        // assert 
+        Assert.AreEqual(indexModel.LengthInMeters, 16_000);
+    }
+}
